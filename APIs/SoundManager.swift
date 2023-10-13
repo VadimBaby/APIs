e01cd6b3e1e47a08d304e1530efc6b62a48226b2
VadimBaby <vadimdim22@gmail.com>
2023-10-13 23:33:03 +0700

    Created SoundManager

--- APIs/SoundManager.swift
+++ APIs/SoundManager.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import AVKit
+
+class SoundManager {
+    static let instanse = SoundManager()
+    
+    var player: AVAudioPlayer?
+    
+    func playSoundFromData(data: Data) throws -> AVAudioPlayer {
+        do {
+            player = try AVAudioPlayer(data: data)
+            
+            guard let player else { throw Errors.playerIsNil }
+            
+            return player
+        } catch {
+            print("that shit")
+            throw error
+        }
+    }
+}
