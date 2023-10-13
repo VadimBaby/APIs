5df75e08f1a5a2b44825ceba90a880ef9f88943b
VadimBaby <vadimdim22@gmail.com>
2023-10-13 23:32:42 +0700

    Created Errors enum in Errors.swift

---
+++ APIs/Errors.swift
@@ -0,0 +1,13 @@
+//
+//  Errors.swift
+//  APIs
+//
+//  Created by Вадим Мартыненко on 13.10.2023.
+//
+
+import Foundation
+
+enum Errors: Error {
+    case JSONSerializationFailder
+    case playerIsNil
+}
---
+++ APIs/SoundManager.swift
@@ -0,0 +1,8 @@
+//
+//  SoundManager.swift
+//  APIs
+//
+//  Created by Вадим Мартыненко on 13.10.2023.
+//
+
+import Foundation
