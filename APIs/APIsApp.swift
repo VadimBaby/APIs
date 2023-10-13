de01de167162c4f6cef319502c3bff919ac32f9f
VadimBaby <vadimdim22@gmail.com>
2023-10-13 23:09:36 +0700

    Init

---
+++ APIs/APIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIsApp.swift
+//  APIs
+//
+//  Created by Вадим Мартыненко on 13.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct APIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
