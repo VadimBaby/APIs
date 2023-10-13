ef79656cfe06ea99d327288e4dad96f5cb5792db
VadimBaby <vadimdim22@gmail.com>
2023-10-13 23:36:42 +0700

    Created SpeechGoogleAPIModel

---
+++ APIs/SpeechGoogleAPI/SpeechGoogleAPIModel.swift
@@ -0,0 +1,12 @@
+//
+//  SpeechGoogleAPIModel.swift
+//  APIs
+//
+//  Created by Вадим Мартыненко on 13.10.2023.
+//
+
+import Foundation
+
+struct SpeechGoogleAPIModel: Codable {
+    let audioStream: Data
+}
