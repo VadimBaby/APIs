3466b112c585474ad74f7cd844b2e70206cb0b07
VadimBaby <vadimdim22@gmail.com>
2023-10-14 20:35:54 +0700

    Created getSectionMessage func

--- APIs/ChatGPT/ChatGPTViewModel.swift
+++ APIs/ChatGPT/ChatGPTViewModel.swift
@@ -44,6 +44,28 @@ final class ChatGPTViewModel: ObservableObject {
         return message
     }
     
+    func getSectionMessage() -> [[MessageModel]] {
+        var res: [[MessageModel]] = []
+        var tmp: [MessageModel] = []
+        
+        for message in messages {
+            if let firstMessage = tmp.first {
+                let daysBetween = firstMessage.date.daysBetween(date: message.date)
+                if daysBetween >= 1 {
+                    res.append(tmp)
+                    tmp.removeAll()
+                    tmp.append(message)
+                } else {
+                    tmp.append(message)
+                }
+            } else {
+                tmp.append(message)
+            }
+        }
+        res.append(tmp)
+        return res
+    }
+    
     private func getEntities() {
         let request = NSFetchRequest<MessageEntity>(entityName: "MessageEntity")
         
