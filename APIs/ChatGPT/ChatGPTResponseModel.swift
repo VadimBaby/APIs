f15709c38326ed31a78a820d2d76e0197aa8b25b
VadimBaby <vadimdim22@gmail.com>
2023-10-14 20:15:55 +0700

    Created ChatGPTResponseModel

---
+++ APIs/ChatGPT/ChatGPTResponseModel.swift
@@ -0,0 +1,14 @@
+//
+//  ChatGPTResponseModel.swift
+//  APIs
+//
+//  Created by Вадим Мартыненко on 14.10.2023.
+//
+
+import Foundation
+
+
+struct ChatGPTResponseModel: Codable {
+    let text: String?
+    let message: String?
+}
