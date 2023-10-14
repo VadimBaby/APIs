9c981080b8c606de254da4272d13ab93ef1968c8
VadimBaby <vadimdim22@gmail.com>
2023-10-14 20:29:45 +0700

    Added ChatGPTManager and CoreDataManager

--- APIs/ChatGPT/ChatGPTViewModel.swift
+++ APIs/ChatGPT/ChatGPTViewModel.swift
@@ -19,4 +19,8 @@ final class ChatGPTViewModel: ObservableObject {
             }
         }
     }
+    
+    private let manager = ChatGPTManagerActor.shared
+    
+    private let coreDataManager = CoreDataManager.instance
 }
