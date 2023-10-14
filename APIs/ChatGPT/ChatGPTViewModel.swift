562dc23d77acb58530a5221bd48e8ea5536b6773
VadimBaby <vadimdim22@gmail.com>
2023-10-14 20:30:45 +0700

    Created getResponse func and addMessage func in ChatGPTViewModel

--- APIs/ChatGPT/ChatGPTViewModel.swift
+++ APIs/ChatGPT/ChatGPTViewModel.swift
@@ -23,4 +23,44 @@ final class ChatGPTViewModel: ObservableObject {
     private let manager = ChatGPTManagerActor.shared
     
     private let coreDataManager = CoreDataManager.instance
+    
+    @ChatGPTManagerActor
+    private func getResponse(text: String) async {
+        do {
+            let data = try await manager.getResponse(text: text, rapidKey: rapidKey)
+            
+            let response = try JSONDecoder().decode(ChatGPTResponseModel.self, from: data)
+            
+            var answerText: String {
+                if let text = response.text {
+                    return text
+                } else if let message = response.message {
+                    return message
+                } else {
+                    return "Error"
+                }
+            }
+            
+            await MainActor.run {
+                let answer: MessageModel = MessageModel(text: answerText, type: .chatgpt)
+                
+                self.messages.append(answer)
+                
+                addMessage(message: answer)
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
+    private func addMessage(message: MessageModel) {
+        let newMessage = MessageEntity(context: coreDataManager.context)
+        
+        newMessage.id = message.id
+        newMessage.text = message.text
+        newMessage.date = message.date
+        newMessage.type = message.type.rawValue
+        
+        coreDataManager.save()
+    }
 }
