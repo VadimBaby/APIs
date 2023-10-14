a75af9fbd3453a271be08bb70547e8a329385c7f
VadimBaby <vadimdim22@gmail.com>
2023-10-14 20:10:28 +0700

    Created CoreDataManager

--- APIs/CoreDataManager.swift
+++ APIs/CoreDataManager.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import CoreData
+
+class CoreDataManager {
+    static let instance = CoreDataManager()
+    
+    let container: NSPersistentContainer
+    let context: NSManagedObjectContext
+    
+    init(){
+        container = NSPersistentContainer(name: "CoreData")
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Error: \(error)")
+            }
+        }
+        context = container.viewContext
+    }
+    
+    func save() {
+        do{
+            try context.save()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+}
